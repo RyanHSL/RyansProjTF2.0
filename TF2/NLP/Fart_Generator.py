2b64a3de03d637087d17854072dbdba2beef975c
RyanHSL <RyanHSL@users.github.com>
2020-08-11 20:57:29 -0400

    Add User Interface
    Add User Interface

--- TF2/NLP/Fart_Generator.py
+++ TF2/NLP/Fart_Generator.py
@@ -11,11 +11,12 @@ import matplotlib.pyplot as plt
 import string
 import requests
 
+
 class LSTMModel(Model):
     def __init__(self, input_shape, V, sequence_length, M=100, D=50):
         super(LSTMModel, self).__init__()
         self.M = M
-        self.D = D # Embedding dimension
+        self.D = D  # Embedding dimension
         self.V = V
         self.sequence_length = sequence_length
 
@@ -25,7 +26,7 @@ class LSTMModel(Model):
         x = LSTM(self.M, activation=tf.nn.relu, return_sequences=True)(x)
         x = GlobalMaxPooling1D()(x)
         x = Dense(self.M, activation=tf.nn.relu)(x)
-        x = Dense(self.V, activation=tf.nn.softmax)(x) # Note: the output length is V
+        x = Dense(self.V, activation=tf.nn.softmax)(x)  # Note: the output length is V
         model = Model(i, x)
         model.summary()
         self.model = model
@@ -36,6 +37,7 @@ class LSTMModel(Model):
         m = self.model(x, training=training)
 
         return m
+
 
 def GRUModel(Model):
     def __init__(self, input_shape, V):
@@ -56,13 +58,15 @@ def GRUModel(Model):
 
         return m
 
+
 def clean_text(doc):
-    tokens = doc.split() # Seperated by white spaces
-    table = str.maketrans("", "", string.punctuation) # Remove punctuation from any string
+    tokens = doc.split()  # Seperated by white spaces
+    table = str.maketrans("", "", string.punctuation)  # Remove punctuation from any string
     tokens = [w.translate(table) for w in tokens]
     tokens = [word.lower() for word in tokens if word.isalpha()]
 
     return tokens
+
 
 def get_data():
     # r = requests.get("https://ocw.mit.edu/ans7870/6/6.006/s08/lecturenotes/files/t8.shakespeare.txt")
@@ -87,7 +91,8 @@ def get_data():
     df = pd.read_csv("Data/Trump_Tweets/trumptweets.csv")
     df.drop(["id", "link", "date", "retweets", "favorites", "mentions", "hashtags", "geo"], axis=1)
     # data = df["content"].values
-    df['content'] = df['content'].replace(r'http\S+', '', regex=True).replace(r'www\S+', '', regex=True).replace(r'https\S+', '', regex=True)
+    df['content'] = df['content'].replace(r'http\S+', '', regex=True).replace(r'www\S+', '', regex=True).replace(
+        r'https\S+', '', regex=True)
     data = df["content"].values
     data = data[-2000:]
     # data = " ".join(data)
@@ -104,6 +109,7 @@ def get_data():
 
     return tokenizer, sequences, v
 
+
 def split_time_series_data(doc, T):
     X, Y = [], []
 
@@ -117,10 +123,11 @@ def split_time_series_data(doc, T):
     Y = np.array(Y)
     N = len(Y)
 
-    X_train, X_test = X[:-N//2], X[-N//2:]
-    Y_train, Y_test = Y[:-N//2], Y[-N//2:]
+    X_train, X_test = X[:-N // 2], X[-N // 2:]
+    Y_train, Y_test = Y[:-N // 2], Y[-N // 2:]
 
     return X_train, X_test, Y_train, Y_test, N
+
 
 def split_data(data, V):
     datalist = []
@@ -130,7 +137,7 @@ def split_data(data, V):
                 datalist.append(d[:i])
     # X_train, Y_train = data[:-N//2, :-1], data[:-N//2, -1]
     # X_test, Y_test = data[-N//2:, :-1]. data[-N//2:, -1]
-    max_length = 20 # "NLP" (and any subsequent words) was ignored because we limit queries to 32 words.
+    max_length = 20  # "NLP" (and any subsequent words) was ignored because we limit queries to 32 words.
     sequences = pad_sequences(datalist, maxlen=max_length, padding='pre')
 
     X = sequences[:, :-1]
@@ -160,7 +167,7 @@ def main():
     # scheduler = tf.keras.callbacks.LearningRateScheduler(schedule)
     # r = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=100, callbacks=[scheduler], batch_size=1024)
     # r = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=100, batch_size=1024)
-    r = model.fit(X, Y, epochs=200, batch_size = 256) # loss: 0.4774 - accuracy: 0.8719
+    r = model.fit(X, Y, epochs=200, batch_size=256)  # loss: 0.4774 - accuracy: 0.8719
 
     plt.plot(r.history["loss"], label="loss")
     # plt.plot(r.history["val_loss"], label="val loss")
@@ -183,31 +190,33 @@ def main():
     # plt.legend()
     # plt.show()
 
-    text_seed = "Make America Great Again"
-    tweet_length = 10
-    for i in range(10):
-        text = []
-        for _ in range(tweet_length):
-            encoded = tokenizer.texts_to_sequences([text_seed])
-            encoded = pad_sequences(encoded, maxlen=X.shape[1], padding='pre')
+    text_seed = input("Enter your tweet topic: ")
+    tweet_length = int(input("Enter the number of lines of your tweet: "))
+    if text_seed is not None and tweet_length > 0:
+        for i in range(10):
+            text = []
+            for _ in range(tweet_length):
+                encoded = tokenizer.texts_to_sequences([text_seed])
+                encoded = pad_sequences(encoded, maxlen=X.shape[1], padding='pre')
 
-            y_pred = np.argmax(model.predict(encoded), axis=-1)
+                y_pred = np.argmax(model.predict(encoded), axis=-1)
 
-            predicted_word = ""
-            for word, index in tokenizer.word_index.items():
-                if index == y_pred:
-                    predicted_word = word
-                    break
+                predicted_word = ""
+                for word, index in tokenizer.word_index.items():
+                    if index == y_pred:
+                        predicted_word = word
+                        break
 
-            text_seed = text_seed + ' ' + predicted_word
-            text.append(predicted_word)
+                text_seed = text_seed + ' ' + predicted_word
+                text.append(predicted_word)
 
-        text_seed = text[-1]
-        text = ' '.join(text)
-        print(text)
+            text_seed = text[-1]
+            text = ' '.join(text)
+            print(text)
 
     model.save_weights("trump")
     return
+
 
 if __name__ == "__main__":
     main()
