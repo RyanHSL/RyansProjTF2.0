67d488211dd2f6fd313753047503b5989c960000
RyanHSL <RyanHSL@users.github.com>
2022-01-11 00:00:53 -0500

    Enhance the chatbot
    Add more topics and add logics for ending a chat.

---
+++ TF2/ChatBot/training.py
@@ -0,0 +1,85 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow import keras
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        documents.append((word, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    word_patterns = doc[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+# train_x = pad_sequences(train_x, maxlen=40)
+
+# model = Sequential()
+# model.add(keras.layers.Embedding(10000, 100, input_length=40))
+# model.add(keras.layers.LSTM(64, return_sequences=True, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(keras.layers.LSTM(32, activation='relu'))
+# model.add(Dense(len(train_y[0]), activation='softmax'))
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# model = RNN(64, len(train_y[0]), num_layers=2)
+
+
+model.compile(optimizer=sgd,
+                loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot.h5', hist)
+model.save('chatbot.model')
